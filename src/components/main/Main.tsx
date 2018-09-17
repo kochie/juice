1dd80fc3269c0d49ca3feb04dbf1061ef7915b1b
Robert Koch <robert@kochie.io>
2018-09-17 18:34:12 +1000

    Initial commit

---
+++ src/components/main/Main.tsx
@@ -0,0 +1,10 @@
+import * as React from 'react'
+
+export default class Main extends React.Component {
+  render() {
+    return(
+      <div>
+        Hello Amy!
+      </div>
+  )}
+}
---
+++ src/index.tsx
@@ -0,0 +1,6 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+
+import {Main} from './components'
+
+ReactDOM.render(<Main />, document.getElementById('app'))
